f570fe32e8b815726667c459894dc0f6246642f0
tomoyukik <kobori.tomoyuki@gmail.com>
2019-10-26 17:09:17 +0900

    fix customers controller
    modified:   app/controllers/customers_controller.rb

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -4,21 +4,24 @@ class CustomersController < ApplicationController
   protect_from_forgery except: %i[show create update]
 
   def show
+    puts params
     @customer = Customer.find_by uuid: params[:uuid]
     @customer ||= Customer.create uuid: params[:uuid]
-    render json: { status: 'SUCCESS', data: @customer.point }
+    render json: { status: 'SUCCESS', point: @customer.point }
   end
 
   def create
+    puts params
     @customer = Customer.create customer_params
-    render json: { status: 'SUCCESS', data: @customer }
+    render json: { status: 'SUCCESS', customer: @customer.uuid }
   end
 
   def update
+    puts params
     @customer = Customer.find_by uuid: params[:uuid]
     @customer ||= Customer.create uuid: params[:uuid]
     new_point = params[:point]
-    @customer.point = new_point
+    @customer.point += new_point
     render json: {
       status: 'SUCCESS', customer: @customer.uuid, point: @customer.point
     }
