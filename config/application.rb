3d51e0966269a047b60c747925b77643af8b6ae2
tomoyukik <kobori.tomoyuki@gmail.com>
2019-10-26 20:15:58 +0900

    update

--- config/application.rb
+++ config/application.rb
@@ -17,15 +17,20 @@ module Maido
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.web_console.whitelisted_ips = '0.0.0.0/0'
+    # config.web_console.whitelisted_ips = '0.0.0.0/0'
 
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins "*"
-        resource "*",
-          headers: :any,
-          methods: [:get, :post, :put, :options, :head]
-      end
-    end
+    # config.middleware.insert_before 0, Rack::Cors do
+    #   allow do
+    #     origins "*"
+    #     resource "*",
+    #       headers: :any,
+    #       methods: [:get, :post, :put, :options, :head]
+    #   end
+    # end
+    config.action_dispatch.default_headers = {
+      'Access-Control-Allow-Credentials' => 'true',
+      'Access-Control-Allow-Origin' => '*',
+      'Access-Control-Request-Method' => '*'
+    }
   end
 end
