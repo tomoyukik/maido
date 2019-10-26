e4e50280ad6d60655f02e2e61e6357acefbb7701
tomoyukik <kobori.tomoyuki@gmail.com>
2019-10-26 20:51:14 +0900

    debug

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :customers, only: %i[show new create update], param: :uuid
+  get 'customers/:uuid/update', to: 'customers#update', param: :uuid
 end
