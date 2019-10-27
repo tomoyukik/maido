165d64bf67965b131459e3aac3c9b349d3caa444
tomoyukik <kobori.tomoyuki@gmail.com>
2019-10-27 14:13:02 +0900

    fix_seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+seed = 0
+9.times do
+  seed += 11_111
+  uuid = seed
+  Customer.create(uuid: uuid)
+end
+
 5.times do
   uuid = Random.rand(11_111..99_999)
   point = Random.rand(1..100)
-  Customer.create(uuid: uuid, point: point)
+  Customer.create(uuid: uuid, point: point) unless Customer.exists?(uuid: uuid)
 end
